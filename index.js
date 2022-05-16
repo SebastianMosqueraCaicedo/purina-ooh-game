8bc5d19cf60b8914d7664a17ac84a3275ffba39f
tabo <1005893565@u.icesi.edu.com>
2022-05-16 14:39:30 -0500

    Establece sintaxis base de servidor

---
+++ index.js
@@ -0,0 +1,81 @@
+// Requerimientos
+const express = require('express');
+const { Server } = require( 'socket.io' );
+const { SerialPort, ReadlineParser} = require('serialport');
+const app = express();
+const httpServer = app.listen(5050);
+const ioServer = new Server(httpServer);
+
+// Usuarios
+const staticDisplay = express.static('public-display');
+const staticMovile = express.static('public-movile');
+app.use('/display', staticDisplay);
+app.use('/movile', staticmovile);
+app.use(express.json());
+
+// info a guardar en servidor
+/* cuanta gente pasa?
+ * cuanta gente coge recompensa
+ * cuanta gente coge codigo
+ * cuanto tienpo se tardan?
+ * estado de la maquina
+*/
+let emailArray = [];
+let arregloMaquinas = [
+	{numero:0, gifts:0, funcional:false}
+]
+
+// cantidad de personas que...
+let cantPasa = 0;
+let cantJuega = 0;
+let cantGift = 0;
+let cantCodigo = 0;
+
+// tiempo que se tardan en completar
+let arregloTiempo = [];
+let tiempoPromedio = 0;
+
+// funciones a comunicar con servidor
+ioServer.on('connection', (socket) => {
+	socket.on('nuevaMaquina', (numeroMaquina) => {
+		arregloMaquinas.push({numero: numeroMaquina, gifts: 50, funcional:false});
+	});
+
+	socket.on('actualizaMaquina', (objetoMaquina) => {
+		// busca la maquina por numero en el arreglo, y la actualiza
+	});
+
+	let tPromedio = () => {
+		if (arregloTiempo.length > 0){
+			let suma = 0;
+			for (let i=0; i < arregloTiempo.length; i++){
+				suma += arregloTiempo[i];
+			}
+			let end = suma / arregloTiempo.length;
+
+			return end;
+		} else {
+			return 0
+		}
+	}
+	// es redundante?
+	tiempoPromedio = tPromedio;
+}
+
+// funciones a comunicar con arduino
+const protocolConfiguration = {
+	path: '/dev/ttyACM0',
+	baudrate: 9600
+}
+
+const port = new SerialPort(protocolConfiguration);
+const parser = port.pipe (new ReadlineParser());
+
+parser.on('data', (data) => {
+
+	let intData = parseInt(data);
+	if (intData === 1) {
+		ioServer.emit('jumping', true);
+	}
+}
+
